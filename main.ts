3006b6e42dfb2a46e2e66038a16de101d76c7251
CAROLINA MARÍA CAMACHO ZAMBRANO <36022220+ccamacho5237@users.noreply.github.com>
2021-05-26 21:13:13 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,60 @@
+let número = 0
+input.onGesture(Gesture.Shake, function () {
+    número = randint(1, 6)
+    basic.showNumber(número)
+    if (número == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+        basic.clearScreen()
+    } else if (número == 2) {
+        basic.showLeds(`
+            . . . . .
+            . . . # .
+            . . . . .
+            . # . . .
+            . . . . .
+            `)
+        basic.clearScreen()
+    } else if (número == 3) {
+        basic.showLeds(`
+            . . . . .
+            . . . # .
+            . . # . .
+            . # . . .
+            . . . . .
+            `)
+        basic.clearScreen()
+    } else if (número == 4) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            . # . # .
+            . . . . .
+            `)
+        basic.clearScreen()
+    } else if (número == 5) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . # . .
+            . # . # .
+            . . . . .
+            `)
+        basic.clearScreen()
+    } else {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . # . # .
+            . # . # .
+            . . . . .
+            `)
+        basic.clearScreen()
+    }
+})
